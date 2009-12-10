299e52f349630ae12c3fd0793ee8dfeb3548441a
selivanovm <selivanovm@selivanovm-desktop.(none)>
2009-12-10 16:58:22 +0300

    build_lib.bat

---
+++ build_lib.bat
@@ -0,0 +1,5 @@
+rem rm export/*
+rem rm librabbitmq.a 
+dmd src/amqp_base.d src/amqp_private.d src/amqp_mem.d src/amqp.d src/amqp_table.d src/amqp_framing.d  src/amqp_framing_.d src/amqp_connection.d src/amqp_socket.d src/amqp_api.d -O -Hdexport -release -lib -oflibrabbitmq
+
+
